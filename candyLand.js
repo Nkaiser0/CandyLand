b69bddcaa82214c7d7546a7e437f5cb7c43fcf70
phamtom808 <dpham@mines.edu>
2019-10-09 21:02:42 -0600

    added dark mode

--- candyLand.js
+++ candyLand.js
@@ -24,6 +24,20 @@ var endOfGame = false;
 var score = 0;
 var scoring = false;
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
 document.onkeyup = keyPress;
 drawCanvas();
 setCandyToFall();
